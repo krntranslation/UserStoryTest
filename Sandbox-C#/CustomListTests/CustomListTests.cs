879accc39e639a079116ea35eadc90336a173f30
unknown <krntranslation@gmail.com>
2019-10-10 08:06:08 -0500

    added test

--- Sandbox-C#/CustomListTests/CustomListTests.cs
+++ Sandbox-C#/CustomListTests/CustomListTests.cs
@@ -11,6 +11,7 @@ namespace CustomListTests
         // unit test for adding multiple items to check Count property
         // unit test for adding number of items beyond 'Capacity' but it still adds
 
+
         [TestMethod]
         public void Add_AddToEmptyList_ItemGoesToIndexZero()
         {
@@ -26,21 +27,75 @@ namespace CustomListTests
             // assert
             Assert.AreEqual(expected, actual);
         }
-
         [TestMethod]
         public void Add_AddItemToList_CountIncrements()
         {
-            // arrange
+            //arrange
             CustomList<int> testList = new CustomList<int>();
             int expected = 1;
             int actual;
 
-            // act
+            //act
             testList.Add(234);
+            actual = testList.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_Mulitple_Numbers_ToList()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+
+            int expected = 2;
+            int actual;
+
+            // act
+            testList.Add(2);
+            testList.Add(3);
+
             actual = testList.Count;
 
             // assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Capacity_Full_So_AddToCapacity()
+        {
+            CustomList<int> testList = new CustomList<int>();
+
+            int expected = 8;
+            int actual;
+
+            //act
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(6);
+            testList.Add(8);
+            testList.Add(10);
+            actual = testList.Capacity;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Make_ListInto_String()
+        {
+            //Arrange
+            CustomList<int> testList = new CustomList<int>();
+
+            string expected = testList.ToString();
+            string actual;
+            //Act
+            testList.Add(1);
+            testList.Add(3);
+            testList.Add(5);
+
+            actual = testList.ToString();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
+    
 }
--- Sandbox-C#/Sandbox/CustomList.cs
+++ Sandbox-C#/Sandbox/CustomList.cs
@@ -9,15 +9,73 @@ namespace Sandbox
     public class CustomList<T>
     {
         private T[] items;
+        public int Size { get; private set; }
+        private int count;
+        private int word;
+        
+        public int Word
+        {
+            get { return word; }
+            set { count = value; }
+        }
+
+        public int Count
+        {
+            get {return count;}
+
+        }
+        private int capacity;
+
+        public int Capacity
+        {
+            get {return capacity;}
+            set {capacity = value;}
+        }
+        public T this[int index]
+        {
+            get {return items[index];}
+            set {items[index] = value;}
+        }
 
         public CustomList()
         {
             items = new T[4];
-        }
+            count = 0;
+            capacity = 4;
 
+        }
         public void Add(T itemToAdd)
         {
+            if (capacity == count)
+            {
+                capacity = (2 * capacity);
+                T[] newItems = new T[capacity];
+                for (int i = 0; i < count; i++)
+                {
+                    newItems[i] = items[i];
+                }
+                items = newItems;
+
+            }
+            items[count] = itemToAdd;
+            count += 1;
+            if(count == 3)
+            {
+              items.ToString();
+                for(int i = 0; i < items.Length; i++)
+                {
+                    items[i].ToString();
+                       
+                }
+                
+            }
+
 
         }
+        
+
+
+
+
     }
 }
